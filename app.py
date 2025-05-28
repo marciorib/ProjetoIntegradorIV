0f7764d9b81d8dabd0988331ceb1140daf720bd8
Marcio Antonio Ribeiro <maribeiro@correios.com.br>
2025-05-28 12:00:15 -0300

    Adiciona formulário de cadastro com Bootstrap

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = 'sua_chave_secreta_aqui'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET', 'POST'])
 def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        telefone = request.form['telefone']
+        # Aqui você pode salvar os dados (em memória, arquivo, banco, etc.)
+        flash('Cadastro realizado com sucesso!', 'success')
+        return redirect(url_for('cadastro'))
     return render_template('cadastro.html')
 
 @app.route('/leitura')
